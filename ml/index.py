f1a34def3efce22b288b42ab78acacd0345487e9
ricozhuthegreat <33135503+ricozhuthegreat@users.noreply.github.com>
2019-09-02 16:17:23 -0400

    Added Documentation to index.py
     - Added documentation and instruction/skeleton code for the index.py file

---
+++ ml/index.py
@@ -0,0 +1,17 @@
+
+import pandas as pd
+import numpy as np
+
+import keras
+
+''' 
+    Take in a csv of data points on supposed note values, and then actual performance values.
+    Then use algorithm to calculate deviation, and then train algorithm via deviated values.
+    Take data feedstream from pipeline on web frontend and then run network
+'''
+
+# Load training data
+
+# Train Model
+
+# Save Model
