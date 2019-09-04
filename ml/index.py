9f57823ada0c04e2069fd0fda2cf8390bd3e6fd4
ricozhuthegreat <33135503+ricozhuthegreat@users.noreply.github.com>
2019-09-04 16:34:13 -0400

    Added OOP Implementation of User Request
     - Added a python OOP implementation of the web (/maybe Arduino, still WIP) request

--- ml/index.py
+++ ml/index.py
@@ -2,6 +2,8 @@
 import pandas as pd
 import numpy as np
 
+import json
+
 import keras
 
 ''' 
@@ -10,15 +12,29 @@ import keras
     Take data feedstream from pipeline on web frontend and then run network
 '''
 
-# Function for loading verified data
-def load_ver (filename):
-    return pd.read_csv('../data/' + filename)
+# Class that represents a user json and file request
+class Request(object):
 
-# Function for translating user data music stream into Pandas dataframe
-def parse_user (filename):
-    return pd.read_csv('../gen/' + filename)
+    def __init__(self, ver_file, user_file, json_req):
+        self.ver_file = load_ver(ver_file)
+        self.user_file = load_user(user_file)
+        self.parse_request = parse_request(json_req)
+
+    def load_ver (filename):
+        # Function for loading verified data
+        return pd.read_csv('../data/' + filename)
+
+    def load_user (filename):
+        # Function for loading user data music stream into Pandas dataframe
+        return pd.read_csv('../gen/' + filename)
+
+    def parse_request (json_req):
+        # Function that parses the json request that the user sends via the web/Arduino interface
+        return json.loads(json_req)
 
 # Load training data
+
+
 
 # Train Model
 
