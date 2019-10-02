5fbfd9afb3b5c7899da05c686b670e5589bf55ca
Rico Zhu <33135503+ricozhuthegreat@users.noreply.github.com>
2019-10-02 18:36:58 -0400

    Update index.py
     - Added documentation
     - Fixed bug with method scoping

--- ml/index.py
+++ ml/index.py
@@ -14,7 +14,10 @@ import keras
 ''' 
     Take in a csv of data points on supposed note values, and then actual performance values.
     Then use algorithm to calculate deviation, and then train algorithm via deviated values.
-    Take data feedstream from pipeline on web frontend and then run network
+    Take data feedstream from pipeline on web frontend and then run network.
+
+    NN/Model used is a comparison network that checks for how similar your playing is to
+    the actual song.
 '''
 
 # Class that represents a user json and file request
@@ -23,17 +26,17 @@ class Request(object):
     def __init__(self, ver_file, user_file, json_req):
         self.ver_file = load_ver(ver_file)
         self.user_file = load_user(user_file)
-        self.parse_request = parse_request(json_req)
+        self.parse_request = parse_requests(json_req)
 
-    def load_ver (filename):
+    def load_ver (self, filename):
         # Function for loading verified data
         return pd.read_csv('../data/' + filename)
 
-    def load_user (filename):
+    def load_user (self, filename):
         # Function for loading user data music stream into Pandas dataframe
         return pd.read_csv('../gen/' + filename)
 
-    def parse_request (json_req):
+    def parse_requests (self, json_req):
         # Function that parses the json request that the user sends via the web/Arduino interface
         return json.loads(json_req)
 
